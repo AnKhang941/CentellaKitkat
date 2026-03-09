67bf2271b2b386eb4f9d994c4b3e9cc2c8c589f2
An Khang Nguyen <ankhangnguyen941@gmail.com>
2026-03-09 12:10:00 +0700

    create project

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+var controller = require(__dirname  + "/apps/controllers");
+app.use(controller);
+app.set("views",__dirname + "/apps/views");
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/apps/public"));
+var server = app.listen(3000, function(){
+   console.log("server is running");
+});
